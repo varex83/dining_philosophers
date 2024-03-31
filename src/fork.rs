96dcc3377851c36d1ab7c2efd963e39f0967f1c9
Bohdan Ohorodnii <limposfeed@gmail.com>
2024-03-31 14:15:24 +0300

    feat: add panic to assure that we can't take taken forks

--- src/fork.rs
+++ src/fork.rs
@@ -5,18 +5,21 @@ pub struct Fork {
 
 impl Fork {
     pub fn new(id: u32) -> Fork {
-        Fork {
-            id,
-            taken: false,
-        }
+        Fork { id, taken: false }
     }
-    
+
     pub fn take(&mut self) {
+        if self.taken {
+            panic!("Fork {} is already taken!", self.id);
+        }
         println!("Fork {} is taken!", self.id);
         self.taken = true;
     }
-    
+
     pub fn put(&mut self) {
+        if !self.taken {
+            panic!("Fork {} is already put!", self.id);
+        }
         println!("Fork {} is put!", self.id);
         self.taken = false;
     }
